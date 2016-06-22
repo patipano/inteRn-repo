e9021d21ee5e5a5695e79cd138f6c7257b51fdc9
patipano <patipan.mana@gmail.com>
2016-06-22 16:47:52 +0700

    Almost complete

--- my-func2.R
+++ my-func2.R
@@ -12,6 +12,8 @@ convertVec2Tri <- function(.vec, .rows, .cols) {
   return(matrix(collection, .rows, .cols, byrow = TRUE))
 }
 
+### 
+
 ### Triangularise vector
 triangularise <- function(.vec) {
   rows <- cols <- length(.vec)
@@ -20,7 +22,7 @@ triangularise <- function(.vec) {
   return(result)
 }
 
-### Get weights triangle for Mack's model (w)
+### Get weights triangle for Mack's model "w"
 getWeights <- function(.tri) {
   rows <- dim(.tri)[1]
   cols <- dim(.tri)[2]
@@ -29,7 +31,7 @@ getWeights <- function(.tri) {
   return(result)
 }
 
-### Calculate volume-weighted average of age-to-age factor (lambda)
+### Calculate volume-weighted average of age-to-age factor "lambda"
 getMackLambda <- function(.tri) {
   w <- getWeights(.tri)
   f <- .tri / w
@@ -38,6 +40,24 @@ getMackLambda <- function(.tri) {
   return(result)
 }
 getMackLambda(data.tri)
+
+### Backward fit triangle with lambda (VWAs)
+fitBackward <- function(.tri, .lambda) {
+  rows <- dim(.tri)[1]
+  cols <- dim(.tri)[2]
+  
+  diagData.tri <- llply(1:rows, function(x) rep(.tri[x, cols - x + 1], 10 - x + 1)) %>%
+    unlist %>% convertVec2Tri(rows, cols)
+  
+  lambda <- .lambda[-1]
+  n <- length(lambda)
+  lambda.cumul <- llply(1:n, function(x) c(rev(cumprod(rev(lambda[1:(n - x + 1)]))), 1)) %>%
+    unlist %>% c(1) %>% convertVec2Tri(rows, cols)
+  
+  result <- diagData.tri / lambda.cumul
+  return(result)
+}
+fitBackward(data.tri, getMackLambda(data.tri))
 
 ### Calculate unscaled Pearson residuals
 getResidUnscaled <- function(.tri) {
@@ -48,20 +68,100 @@ getResidUnscaled <- function(.tri) {
   result <- sqrt(w) * (f - lambda)
   return(result)
 }
+getResidUnscaled(data.tri)
 
-### Calculate square-root of scale parameters for each period (sigma)
+### Calculate square-root of scale parameters for each period "sigma"
 getMackSigma <- function(.tri, .finalVariance = FALSE) {
   resid <- getResidUnscaled(.tri)
   result <- colSums(resid ^ 2, na.rm = TRUE)
   result <- llply(1:dim(.tri)[2],
     function(x) result[x] / (sum(is.finite(resid[,x]), na.rm = TRUE) - 1)
   ) %>% unlist
+  
+  if (!.finalVariance)
+    result[length(result)] <- NA
+  else
+    result[length(result)] <- min(rev(result)[2:3])
+  
   return(sqrt(result))
 }
+getMackSigma(data.tri, TRUE)
 
 ### Calculate scaled Pearson residuals, used in resampling
-getResidualScaled <- function(.tri, .finalVariance = FALSE) {
-  sigma.tri <- triangularise(getMackSigma(.tri, .finalVariance = .finalVariance))
+getResidScaled <- function(.tri, .finalVariance = FALSE) {
+  sigma.tri <- triangularise(getMackSigma(.tri, .finalVariance))
   result <- getResidUnscaled(.tri) / sigma.tri
   return(result)
+}
+getResidScaled(data.tri)
+
+##########################################################################
+
+### Resample scaled residuals
+resampleResid <- function(.tri, .finalVariance = TRUE) {
+  resid.vec <- getResidScaled(.tri, .finalVariance) %>%
+    as.vector %>%
+    extract(is.finite(.))
+  result <- sample(resid.vec, size = length(resid.vec) + 1, replace = TRUE) %>%
+    convertVec2Tri(dim(.tri)[1] - 1, dim(.tri)[2] - 1) %>%
+    rbind(rep(NA, dim(.tri)[2] - 1)) %>%
+    cbind(rep(NA, dim(.tri)[1]), .)
+  return(result)
+}
+resampleResid(data.tri)
+
+### Recover (pseudo) data from scaled residuals
+recoverData <- function(.tri, .resid, .finalVariance = TRUE) {
+  w <- getWeights(.tri)
+  sigma <- triangularise(getMackSigma(.tri, .finalVariance))
+  lambda <- triangularise(getMackLambda(.tri))
+  
+  f <- .resid * sigma / sqrt(w) + lambda
+  lambda.fit <- colSums(w * f, na.rm = TRUE) / colSums(w, na.rm = TRUE)
+  result <- fitBackward(.tri, lambda.fit)
+  return(result)
+}
+recoverData(data.tri, resampleResid(data.tri))
+getMackLambda(data.tri)
+
+### Forecast triangle with process error
+forecastMack <- function(.tri) {
+  rows <- dim(.tri)[1]
+  cols <- dim(.tri)[2]
+  diagData <- laply(1:rows, function(x) .tri[x, cols - x + 1]) %>% rev
+  lower <- llaply(2:rows,
+    function(x) {
+      )
+  return(diagData)
+}
+forecastMack(data.tri)
+data.tri
+# ### Calculate reserves for each origin period
+# getReserve <- function(.tri, .lambda) {
+#   rows <- dim(.tri)[1]
+#   cols <- dim(.tri)[2]
+#   lambda.cumul <- rev(cumprod(rev(c(.lambda[-1], 1))))
+#   
+#   diagData <- laply(1:rows, function(x) .tri[x, cols - x + 1]) %>% rev
+#   projData <- diagData*lambda.cumul
+#   result <- as.matrix(rev(projData - diagData))
+#   
+#   colnames(result) <- "Reserve"
+#   rownames(result) <- 1:rows
+#   return(result)
+# }
+# getReserve(data.tri, getMackLambda(data.tri))
+
+##########################################################################
+
+bootMack <- function(.tri, B = 5) {
+  llply(1:B,
+    function(x) {
+      bootResid.tri <- resampleResid(.tri, .finalVariance = TRUE)
+      bootData.tri <- recoverData(.tri, resid.tri, .finalVariance = TRUE)
+      bootLambda <- getMackLambda(bootData.tri)
+      bootReserve <- getReserve(bootData.tri,
+      
+    } 
+  )
 }
